faa867580d866a6ddaa8d40a93ca772bd65f577a
Qmun14 <mamunramdhan@gmail.com>
2023-02-14 18:02:21 +0700

    Menambahkan fitur --Cari Robot..--

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Cardlist from './Cardlist';
 import SearchBox from './SearchBox';
 import { robots } from './robots';
 
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            robots: robots,
+            searchField: ''
+        }
+    }
 
-const App = () => {
-    return (
-        <div className='tc'>
-            <h1>Robofriends</h1>
-            <SearchBox />
-            <Cardlist robots={robots} />
-        </div>
-    )
+    onSearchChange = (e) => {
+        this.setState({ searchField: e.target.value });
+    }
+
+    render() {
+        const filteredRobots = this.state.robots.filter((robot) => {
+            return robot.name
+                .toLowerCase()
+                .includes(this.state.searchField.toLowerCase());
+        });
+        return (
+            <div className="tc">
+                <h1>Robofriends</h1>
+                <SearchBox searchChange={this.onSearchChange} />
+                <Cardlist robots={filteredRobots} />
+            </div>
+        );
+    }
 }
 
 export default App;     
