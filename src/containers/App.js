b47e0a5c5ef13de453e2db596c5c05789e273e44
Qmun14 <mamunramdhan@gmail.com>
2023-02-15 11:08:45 +0700

    Reafctoring code ke functional concept dan memanfaatkan API react Hook

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import Cardlist from '../components/Cardlist';
 import Scrollbar from '../components/Scrollbar';
 import SearchBox from '../components/SearchBox';
@@ -6,27 +6,34 @@ import SearchBox from '../components/SearchBox';
 import './App.css'
 
 
-class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            robots: [],
-            searchField: ''
-        }
-    }
+const App = () => {
+    // constructor() {
+    //     super();
+    //     this.state = {
+    //         robots: [],
+    //         searchField: ''
+    //     }
+    // }
+    const [robots, setRobots] = useState([]);
+    const [searchField, setSearchField] = useState('');
 
-    componentDidMount() {
+    // componentDidMount() {
+    //     fetch('https://jsonplaceholder.typicode.com/users')
+    //         .then(response => response.json())
+    //         .then(users => this.setState({ robots: users }));
+    // }
+
+    useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(response => response.json())
-            .then(users => this.setState({ robots: users }));
+            .then(users => setRobots(users));
+    }, []);
+
+    const onSearchChange = (e) => {
+        setSearchField(e.target.value);
     }
 
-    onSearchChange = (e) => {
-        this.setState({ searchField: e.target.value });
-    }
-
-    render() {
-        const { robots, searchField } = this.state;
+    {
         const filteredRobots = robots.filter((robot) => {
             return robot.name
                 .toLowerCase()
@@ -39,7 +46,7 @@ class App extends Component {
             (
                 <div className="tc">
                     <h1 className='f2'>Robofriends</h1>
-                    <SearchBox searchChange={this.onSearchChange} />
+                    <SearchBox searchChange={onSearchChange} />
                     <Scrollbar>
                         <Cardlist robots={filteredRobots} />
                     </Scrollbar>
